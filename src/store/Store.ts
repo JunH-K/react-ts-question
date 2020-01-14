2d150a46cae8adc97e314c4823758569790fa3ce
JunH <k.developer87@gmail.com>
2020-01-14 16:54:19 +0900

    [fixed] : local storage null 참조오류

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,18 +1,19 @@
 export default class Store {
     localStorage: Storage;
 
-    constructor(name: string, callback?: () => {}) {
+    constructor(name: any, callback?: () => {}) {
         this.localStorage = window.localStorage;
         this.getLocalStorage(name) || this.setLocalStorage(name, {});
 
         callback && callback();
     }
 
-    getLocalStorage(name: string) {
-        return JSON.parse(this.localStorage.getItem(name) || '');
+    getLocalStorage(name: any) {
+        const item = this.localStorage.getItem(name);
+        return item ? JSON.parse(item) : {};
     }
 
-    setLocalStorage(name:string, item:Object) {
+    setLocalStorage(name: any, item: any) {
         this.localStorage.setItem(name, JSON.stringify(item));
     }
 };
